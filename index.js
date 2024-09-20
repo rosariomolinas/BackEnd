eb9213402b26fa5cb6a2f83231d8dbff2f371339
rosariomolinas <130401216+rosariomolinas@users.noreply.github.com>
2024-09-19 22:56:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var bodyParser = require("body-parser");
 var path = require('path');
 var app = express(); 
 
+require('dotenv').config();
+
+
 const allRoutes = require("./src/routes/index.js")
 
 const dbMongo = require("./src/db/dbconnect")
@@ -30,7 +33,7 @@ app.use('/robiotics', allRoutes);
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || process.env.port, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
  });
