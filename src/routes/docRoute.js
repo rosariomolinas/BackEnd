fb13c8685a7cfdb8e255a521474807d91d1765cd
rosariomolinas <130401216+rosariomolinas@users.noreply.github.com>
2024-09-26 00:00:59 -0300

    Add files via upload

--- src/routes/docRoute.js
+++ src/routes/docRoute.js
@@ -2,7 +2,12 @@ const express = require("express");
 const docController = require("../controllers/doctorController");
 const docRouter = express.Router();
 docRouter.route("/addnew").post(docController.addnew  );
-docRouter.route("/findone").post(docController.findone  );
-docRouter.route("/traertodos").post(docController.traertodos  );
+docRouter.route("/findcode").post(docController.findCode  );
+docRouter.route("/findname").post(docController.findName  );
+docRouter.route("/update").post(docController.update  );
+docRouter.route("/next").post(docController.next  );
+docRouter.route("/previous").post(docController.previous  );
+docRouter.route("/remove").post(docController.remove  );
+
 
 module.exports = docRouter;
--- src/routes/orgRoute.js
+++ src/routes/orgRoute.js
@@ -2,7 +2,8 @@ const express = require("express");
 const orgController = require("../controllers/orgController");
 const orgRouter = express.Router();
 orgRouter.route("/addnew").post(orgController.addnew  );
-orgRouter.route("/findone").post(orgController.findone  );
-orgRouter.route("/traertodos").post(orgController.traertodos  );
-
+orgRouter.route("/findCode").post(orgController.findCode  );
+orgRouter.route("/update").post(orgController.update);
+// orgRouter.route("/update2").post(orgController.update2 );
+// orgRouter.route("/update").post(orgController.update);
 module.exports = orgRouter;
