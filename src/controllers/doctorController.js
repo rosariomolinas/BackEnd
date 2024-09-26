e4ada49afed2ec30bee412045bc051a5c7cd4bc3
rosariomolinas <130401216+rosariomolinas@users.noreply.github.com>
2024-09-25 23:59:48 -0300

    Add files via upload

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -3,32 +3,37 @@ const doctor = require("../db/doctor")
 const addnew =  (req, res) =>
     {
 
-        console.log("Nuevo doctor..." + req.body.nombre + " - " + req.body.edad);
-        organos.findOne({} ).sort({ code : -1}).then((resultado) => 
+        console.log("Nuevo medico..." + req.body.nombre + " - " + req.body.nromatricula);
+        doctor.findOne({} ).sort({ code : -1}).then((resultado) => 
              { if (resultado==null) 
                  { 
-                    organos.create({code: 1, nombre :  req.body.nombre , edad:  req.body.edad} )
+                    doctor.create({code: 1, nombre :  req.body.nombre , apellido : req.body.apellido, matricula :  req.body.nromatricula, activo : true} )
 
                  } 
                  else 
                  { 
                     let newcod = resultado.code + 1;
-                    organos.create({code: newcod, nombre :  req.body.nombre , edad:  req.body.edad} )
+                    doctor.create({code: newcod, nombre :  req.body.nombre , apellido :  req.body.apellido,  matricula:  req.body.nromatricula, activo : true} )
                  } 
     
               });
         
         res.status(200).json({ status: "success" })
     }
-const findone =  (req, res) =>
+const findCode =  (req, res) =>
         {
     
-            console.log("Buscar doctor...");
-            console.log("1",req.body.code);
-            doctor.findOne({"code" : req.body.code} ).then((resultado) => 
+            console.log("Buscar medico...");
+                        
+            let  record = { ...req.body }
+            console.log('req.body', record)
+
+//            console.log("1",req.body.code);
+            console.log("fin buscar medico...");
+            doctor.findOne({"code" : record.code} ).then((resultado) => 
                 { if (resultado==null) 
                     { 
-                       // not found organos.create({code: 1, nombre :  req.body.nombre , edad:  req.body.edad} )
+                       // not found doctor.create({code: 1, nombre :  req.body.nombre , nromatricula:  req.body.nromatricula} )
                        res.status(200).json({ status: "not found" })
                     } 
                     else 
@@ -41,22 +46,129 @@ const findone =  (req, res) =>
                 
         } 
     
+            
+            const findName =  (req, res) =>
+                {
+            
+                    console.log("Find by name...");
+                    doctores.findOne({"nombre" : req.body.nombre} ).then((resultado) => 
+                        { if (resultado==null) 
+                            { 
+                              // not found doctores.create({code: 1, nombre :  req.body.nombre , matricula:  req.body.matricula} )
+                                res.status(200).json({ status: "not found" })
+                            } 
+                            else 
+                            { 
+                                console.log(resultado.nombre);
+                                res.status(200).json(resultado)
+                            } 
+                        });
+                        
+                        
+                }   
+                const update =  (req, res) =>
+
+                    {
+                    
+                        let mensaje = '';
+                        console.log("Modificar medico..." + req.body);
+                    
+                        
+                        const filter = { code : req.body.code, activo : true };
+                        const updatev = req.body;
+                        doctor.updateOne (filter, updatev ).then((result) => 
+                            {
+                                console.log("resultado modificación ", result);
+                                if (result.modifiedCount)
+                                {
+                                    res.status(200).json(result)
+                                    mensaje = "Medico actualizado";
+                                }
+                                else
+                                {
+                                    res.status(200).json(result)
+                                    mensaje = "Medico no actualizado";
+                    
+                                }
+                                
+                                
+                            })
+                        
+
+                        }
+
+
+                        const remove =  (req, res) =>
+    
+                            {
+                        
+                                let mensaje = '';
+                                console.log("Eliminar (desactivar) medico..." + req.body.nombre + " - " + req.body.matricula);
+                    
+                                console.log("Medico deactivado" , req.body.code);
+                                const filter = { code : req.body.code };
+                                const updatev = {activo :  false};
+                                doctor.updateOne (filter, updatev ).then((result) => 
+                                    {
+                    
+                                        mensaje = "Medico desactivado";
+                                        res.status(200).json({ status: mensaje })
+                                    })
+                                
+                    
+                                
+                                
+                            } 
+
+                            const next =  (req, res) =>
+                                {
+                            
+                                    console.log("Siguiente medico...");
+                                                
+                                    console.log("actual=>",req.body.code);
+                                    
+                                    doctor.find({code: {$gt: req.body.code}, activo : true}).sort({code: 1 }).limit(1).then((resultado) => 
+                                        { if (resultado==null) 
+                                            { 
+                                                 console.log("null");
+                                               // not found doctor.create({code: 1, nombre :  req.body.nombre , edad:  req.body.matricula} )
+                                               res.status(200).json({ status: "not found" })
+                                            } 
+                                            else 
+                                            { 
+                                                console.log("found ", resultado);
+                                                res.status(200).json(resultado)
+                                            } 
+                                        });
+                                        
+                                        
+                                } 
+                          
+                                const previous =  (req, res) =>
+                                    {
+                                
+                                        console.log("Anterior medico...");
+                                                    
+                                       //            console.log("1",req.body.code);
+                                        
+                                        doctor.find({code: {$lt: req.body.code}, activo : true}).sort({code: -1 }).limit(1).then((resultado) => 
+                                            { if (resultado==null) 
+                                                { 
+                                                   // not found doctor.create({code: 1, nombre :  req.body.nombre , matricula:  req.body.matricula} )
+                                                   res.status(200).json({ status: "not found" })
+                                                } 
+                                                else 
+                                                { 
+                                                    console.log(resultado);
+                                                    res.status(200).json(resultado)
+                                                } 
+                                            });
+                                            
+                                            
+                                    }
+
+                module.exports = {
+                    addnew, findCode, findName, update, next, previous, remove  
+                }
 
         
-const traertodos =  (req, res) =>
-{
-
-    console.log("todos los doctores..." );
-    doctor.find({}).then((resultado) => 
-            {  
-                res.status(200).json(resultado)
-
-            });
-    
-   
-}
-
-module.exports = {
-    addnew, findone, traertodos
-    }
-    
