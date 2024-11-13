2db62e52f979851837b6946a5bb55fab19da2a4d
rosariomolinas <130401216+rosariomolinas@users.noreply.github.com>
2024-11-13 14:13:43 -0300

    Add files via upload

--- src/db/usuarios.js
+++ src/db/usuarios.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const usuSchema = new mongoose.Schema( {
     user     :  String,
     password :  String,
-    profile  :  String
+    profile  :  String,
+    token    : String
 })
 
 const usuarios = new mongoose.model ('usuarios', usuSchema)
